3e48027a7cbe7a89abbd637a327273862758f14c
Elina Semenko <lavrextra@gmail.com>
2020-11-19 22:45:21 +0200

    Initial Commit

---
+++ test-app-movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-app-movie
+//
+//  Created by Элина on 19.11.2020.
+//  Copyright © 2020 Элина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
