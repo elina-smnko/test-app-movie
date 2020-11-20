60a86f2943ddd0049b44b98c9af6d94b55b41dbb
Elina Semenko <lavrextra@gmail.com>
2020-11-20 23:33:06 +0200

    first vc

---
+++ test-app-movie/Model/Storage.swift
@@ -0,0 +1,9 @@
+//
+//  Storage.swift
+//  test-app-movie
+//
+//  Created by Элина on 20.11.2020.
+//  Copyright © 2020 Элина. All rights reserved.
+//
+
+import Foundation
---
+++ test-app-movie/View/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  test-app-movie
+//
+//  Created by Элина on 20.11.2020.
+//  Copyright © 2020 Элина. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
