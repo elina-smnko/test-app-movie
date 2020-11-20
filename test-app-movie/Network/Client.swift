65323a4c49cfa84e166107962b788ed8172a0906
Elina Semenko <lavrextra@gmail.com>
2020-11-20 12:44:56 +0200

    logic and network

---
+++ test-app-movie/Network/Client.swift
@@ -0,0 +1,9 @@
+//
+//  Client.swift
+//  test-app-movie
+//
+//  Created by Элина on 19.11.2020.
+//  Copyright © 2020 Элина. All rights reserved.
+//
+
+import Foundation
---
+++ test-app-movie/ViewModel/DetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsViewController.swift
+//  test-app-movie
+//
+//  Created by Элина on 19.11.2020.
+//  Copyright © 2020 Элина. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
